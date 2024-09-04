13870bfe1885dcca49421a73f9e0fd047f041199
phamductai12425 <ductaitv49@gmail.com>
2024-09-04 16:33:03 +0700

    Update Book.java
    update

--- Practice_2/Code_1_2/main/java/com/mycompany/app/Book.java
+++ Practice_2/Code_1_2/main/java/com/mycompany/app/Book.java
@@ -1,17 +1,25 @@
 public class Book {
-
-	String title;
-	String author;
-	int numPages;
-	Book (){};
-	public Book(String t, String a, int p) {
-		title = t;
-		author = a;
-		numPages = p;
-	}
-	
-	public static void main(String[] args) {
-		Book myObj = new Book("a", "b", 2);
-		System.out.println(myObj.title);
-	}
+ String title;
+ String author;
+ int numPages;
+ Book() { } ; // default constructor
+ public Book(String t, String a, int p) {
+ title = t;
+ author = a;
+ numPages = p;
+ }
+ public static void main(String[] args) {
+ Book myObj = new Book("a","b",2); 
+ System.out.println(myObj.title);
+ }
+}
+class Book {
+ String title;
+ String author;
+ int numPages;
+ Book() { } 
+ Book(String t, String a, int p) {
+ title = t;
+ author = a;
+ numPages = p;
 }
