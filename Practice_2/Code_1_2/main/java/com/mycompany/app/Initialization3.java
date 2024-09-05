4badfd12c52c89bcbeff5d2367d825d730a23cc6
phamductai12425 <ductaitv49@gmail.com>
2024-09-05 22:57:27 +0700

    Create Initialization3.java

---
+++ Practice_2/Code_1_2/main/java/com/mycompany/app/Initialization3.java
@@ -0,0 +1,8 @@
+public class Initialization3 {
+    public Initialization3() {
+        System.out.println("Ham tao mac dinh duoc tao");
+    }
+    public static void main(String[] args) {
+        Initialization3 obj = new Initialization3();
+    }
+}
