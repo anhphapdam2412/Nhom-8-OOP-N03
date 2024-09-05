a6ff2989174f9635497b5fad675b94e816699544
phamductai12425 <ductaitv49@gmail.com>
2024-09-05 22:58:30 +0700

    Create Initializatione5.java

---
+++ Practice_2/Code_1_2/main/java/com/mycompany/app/Initializatione5.java
@@ -0,0 +1,10 @@
+public class Initializatione5 {
+
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.bark();
+        dog.bark(5d);
+        dog.bark("cia du");
+        dog.bark((int)4);
+    }
+}
