059f03b542f0e2f716745f1ec7ba76382a64049f
phamductai12425 <ductaitv49@gmail.com>
2024-09-17 10:17:59 +0700

    Add files via upload

---
+++ main/java/com/mycompany/app/Transmogrify.java
@@ -0,0 +1,8 @@
+public class Transmogrify {
+    public static void main(String[] args) {
+        Stage s = new Stage();
+        s.go();       // Happy Actor
+        s.change();   // Thay đổi diễn viên
+        s.go();       // Sad Actor
+    }
+}
