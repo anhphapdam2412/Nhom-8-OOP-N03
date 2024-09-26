69ccbea8f9aceb4f069c1bd8219a555e32901968
Anh Phap <phapdamanh@gmail.com>
2024-09-26 17:40:25 +0700

    Chinh sua chuc nang kiem tra nhap

--- CuoiKyOOP/QLHS/src/main/java/com/qlhs/qlhs/Controller/HopThoai.java
+++ CuoiKyOOP/QLHS/src/main/java/com/qlhs/qlhs/Controller/HopThoai.java
@@ -6,7 +6,7 @@ public class HopThoai {
     public static void baoLoi(String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Thông báo");
-        alert.setHeaderText(null); // Không cần tiêu đề
+        alert.setHeaderText("Lỗi"); // Không cần tiêu đề
         alert.setContentText(message); // Nội dung thông báo
 
         alert.showAndWait(); // Hiển thị hộp thoại và chờ người dùng phản hồi
